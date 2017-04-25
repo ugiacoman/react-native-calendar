a24fbd5f720f16a3712c00fbbbb20f4db709853e
Ulises Giacoman <ulises.giacoman@gmail.com>
2017-04-24 23:09:53 -0400

    Add example sectionList

--- src/calendar.js
+++ src/calendar.js
@@ -1,14 +1,40 @@
 /* @flow */
 
 import React from 'react'
-import { Text } from 'react-native'
+import { View, Text, SectionList, StyleSheet } from 'react-native'
 
 export default class Calendar extends React.Component {
+  _renderItem = ({item}) => {
+    return (
+      <View key={item}>
+        <Text key>
+          {item.title}
+        </Text>
+      </View>
+    )
+  }
+
   render () {
     return (
-      <Text>
-        hi
-      </Text>
+      <View style={styles.container}>
+        <SectionList
+          keyExtractor={(item) => item.title}
+          SectionSeparatorComponent={() =>
+            <View style={{height: 1, backgroundColor: 'red'}} />
+          }
+          sections={[ // heterogeneous rendering between sections
+            {data: [{'title': 'apple'}], key: 'dog', renderItem: this._renderItem},
+            {data: [{'title': 'homie'}, {'title': 'dog'}], key: 'hello', renderItem: this._renderItem},
+            {data: [{'title': 'lets do it'}, {'title': 'whatup'}], key: 'month', renderItem: this._renderItem}
+          ]}
+        />
+      </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    top: 20
+  }
+})
