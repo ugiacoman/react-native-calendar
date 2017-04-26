501cf82060755e7838690ce6df68f4f58959c1cf
ugiacoman <ulises.giacoman@forrent.com>
2017-04-26 18:05:44 -0400

    Add highlight

--- src/day.js
+++ src/day.js
@@ -2,24 +2,33 @@ import React from 'react'
 import { TouchableHighlight, Text, View, StyleSheet } from 'react-native'
 
 export default class Day extends React.Component {
-  styles = {
-    container: {
-      width: this.props.size.day.width,
-      height: this.props.size.day.height
-    },
-    text: {
-      textAlign: 'center',
-      fontSize: this.props.size.day.fontSize
+  constructor(props) {
+    super(props)
+    this.state = {
+      highlight: false
     }
+  }
+  _onPressButton = () => {
+    this.setState({highlight: !this.state.highlight})
   }
 
   render () {
     if (this.props.data.day === 0) {
       return (<View style={this.styles.container} />)
     }
+
+    let active
+    if (this.state.highlight) {
+      active = {
+        backgroundColor: '#d3d3d3'
+      }
+    }
     return (
       <View style={this.styles.container}>
         <TouchableHighlight
+        onPress={this._onPressButton}
+        underlayColor={'#d3d3d3'}
+        style={[this.styles.touch, active]}
         >
           <Text style={this.styles.text}>
             {this.props.data.day}
@@ -27,5 +36,22 @@ export default class Day extends React.Component {
         </TouchableHighlight>
       </View>
     )
+  }
+
+  styles = {
+    container: {
+      width: this.props.size.day.width,
+      height: this.props.size.day.height,
+    },
+    text: {
+      top: 8,
+      textAlign: 'center',
+      height: this.props.size.day.height,
+      fontSize: this.props.size.day.fontSize
+    },
+    touch: {
+      borderRadius: this.props.size.day.height,
+      overflow: 'hidden'
+    }
   }
 }
